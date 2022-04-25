e7a5056956c77816e9df2bbc386f1d5682903c11
juanco2597 <juanparra2517@gmail.com>
2022-04-25 11:50:21 -0500

    clases y metodos

---
+++ app.js
@@ -0,0 +1,45 @@
+//Definicion de las clases
+
+class Libro{
+    constructor(titulo, autor, isbn){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+    }
+}
+
+class UI{
+    static mostrarLibros(){
+
+    }
+
+    static agregarLibroLista(libro){
+
+    }
+
+    static eliminarLibro(){
+
+    }
+
+    static mostrarAlerta(mensaje){
+
+    }
+
+    static limpiarCampos(){
+
+    }
+}
+
+class Datos{
+    static traerLibros(){
+
+    }
+
+    static agregarLibro(libro){
+
+    }
+
+    static removerLibro(isbn){
+        
+    }
+}
